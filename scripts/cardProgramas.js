7a664e1121954be85c659d72f98e78609d353d4d
Silva <dtales15@gmail.com>
2023-02-06 21:37:24 -0300

    incluindo orgs com o logo

--- scripts/cardProgramas.js
+++ scripts/cardProgramas.js
@@ -1,41 +1,190 @@
 var cardObject = [
     {
-        title: 'WoMakersCode',
-        image: "https://womakerscode.org/assets/images/cover-front.jpg",
-        describe: "Impulsiona o protagonismo feminino na tecnologia, através de capacitação, mentoria e empregabilidade",
-        site: "https://womakerscode.org/"
+        title: `Asas`,
+        image: `asas.jpeg`,
+        describe: `Asas- Limeira Somos o Asas, uma organização estudantil, sem fins lucrativos, que tem
+        como missão encorajar jovens a serem agentes de mudança por meio de métodos de
+        educação não tradicionais. O nosso propósito é tornar a cidade de Limeira mais
+        consciente, esperançosa e atuante em sua realidade.<br>
+        
+        Atualmente, por causa da pandemia, atuamos no formato online aplicando oficinas
+        quinzenalmente em 4 escolas públicas da cidade de Limeira. Nosso público alvo são os
+        alunos dos 9° anos, com quem trabalhamos aplicando temas que nem sempre são
+        abordados em sala de aula, como: Protagonismo Juvenil, Inteligência Emocional, Vícios
+        e Preconceito.<br>
+        
+        E é assim que impulsionamos pessoas! Fortalecendo as Asas de cada aluno para que
+        eles façam seu próprio voo.<br>
+        
+        No Asas, nos desenvolvemos e temos a oportunidade de crescer em um ambiente
+        comprometido e profissional, mas que também promove momentos leves e
+        divertidos."<br>
+        
+        Estamos com o Processo Seletivo aberto, até dia 26/03, para alunos da FCA e
+        da FT!`,
+        site: ``,
+        facebook: `https://www.facebook.com/Asas.limeira`,
+        email: ``,
+        instagram: `https://www.instagram.com/asas.limeira`,
     },
     {
-        title: 'RePrograma',
-        image: "https://t.ctcdn.com.br/s6P5G9z8A9DofK4cEFI6ZDzO8YU=/1211x681/smart/filters:format(webp)/i422105.png",
-        describe: "Reduzindo o gap de gênero no setor de tecnologia por meio da educação.",
-        site: "https://reprograma.com.br/"
-    },
-    {
-        title: 'PrograMaria',
-        image: "https://www.projetodraft.com/wp-content/uploads/2020/02/01-face-ianachan.jpg",
-        describe: "Empodera Mulheres através da tecnologia diminuindo o gap de gênero no mercado de trabalho.",
-        site: "https://www.programaria.org/"
-    },
-    {
-        title: 'PyLadies Brasil',
-        image: "https://staging-brasil-pyladies.herokuapp.com/images/locais/location.png",
-        describe: "Comunidade mundial que foi trazida ao Brasil com o propósito de instigar mais mulheres a entrarem na área tecnológica.",
-        site: "https://brasil.pyladies.com/"
+        title: `Atria Júnior`,
+        image: `atria.png`,
+        describe: `Por que fazer parte da Atria Júnior?<br>
 
+        Por que entrar na Atria Júnior ainda no primeiro ano de graduação?
+        A Atria Júnior proporciona aos os membros uma vivência empresarial durante
+        a graduação e permite que os conteúdos teóricos sejam aplicados em projetos reais
+        de mercado. Além disso, possibilita criar conexões regionais e nacionais para ampliar
+        o networking e desenvolver habilidades pessoais e profissionais.<br>
+        
+        Algumas das empresas que nossos ex juniores estão hoje: Honda, Burger King,
+        Itaú, Dpaschoal, Bosch.<br>
+        
+        O que é Atria Júnior?<br>
+        
+        A Atria Jr. é uma empresa júnior (EJ) formada por estudantes dos cursos de
+        graduação da Faculdade de Tecnologia da UNICAMP (FT). Desde 2018, a empresa júnior
+        da FT já impactou mais de R$50.000 na economia brasileira através dos seus mais de
+        20 projetos. Esses projetos impactam na vida de clientes e desenvolvem alunos do
+        campus através de experiências alinhadas com a prática de mercado.<br>
+        
+        O que é uma empresa Júnior?<br>
+        
+        As Empresa Juniores são associações civis sem fins lucrativos e com fins
+        educacionais formada exclusivamente por alunos do ensino superior, regulamentada
+        no Brasil através da Lei 13.267/2016, com o propósito de realizar projetos e serviços
+        que contribuam para o desenvolvimento acadêmico e profissional dos associados,
+        capacitados para o mercado de trabalho. As empresas juniores fazem parte do
+        Movimento Empresa Júnior (MEJ). O MEJ tem como missão formar, por meio da
+        vivência empresarial, empreendedores comprometidos e capazes de transformar o
+        Brasil. Desde 2010, o MEJ já impactou mais de R$70.000.000,00 na economia brasileira,
+        que são integralmente reinvestidos na educação empreendedora dos
+        estudantes.`,
+        site: `atriajr.com.br`,
+        facebook: `https://www.facebook.com/jr.Atria`,
+        email: ``,
+        instagram: `https://www.instagram.com/atriajr`,
     },
     {
-        title: 'Django Girls',
-        image: "https://raw.githubusercontent.com/olasitarska/djangogirls/gh-pages/resources/graphics/logo_square.png",
-        describe: "Django Girls é uma organização sem fins lucrativos e uma comunidade que capacita e ajuda as mulheres a organizar oficinas de programação gratuitas.",
-        site: "https://djangogirls.org/pt-br/"
+        title: `CVU`,
+        image: `cvu.svg`,
+        describe: `"O CVU (Centro de Voluntariado Universitário) é uma associação sem fins lucrativos,
+        composta por estudantes da Unicamp. Nosso objetivo é intermediar o ambiente
+        universitário e o voluntariado. Fazemos diversas ações, projetos e até consultorias
+        sociais!"<br>
+
+        O projeto consiste em reunir e agrupar todo material possível
+        relacionado a faculdade, as matérias nas quais as pessoas têm
+        têm maior dificuldade, tutoriais sobre como realizar matrícula e
+        utilizar o GDE, vídeos de apresentação de diversas organizações,
+        dúvidas sobre intercâmbio, iniciação científica e muito mais.<br>
+        
+        E como juntamos nossos materiais e levamos a vocês?
+        No link da nossa bio do instagram você é redirecionado para um site, no qual você
+        poderá escolher, dentre as matérias que nós já disponibilizamos, qual você deseja
+        acessar para obter resumos e conteúdos. Basta clicar e acessar.
+        No mesmo link, existe também a opção de nos falar uma matéria que acreditem ser
+        importante para postarmos conteúdo sobre, e juntos, ajudarmos uns aos outros.`,
+        site: ``,
+        facebook: `https://www.facebook.com/cvulimeira`,
+        email: ``,
+        instagram: `https://www.instagram.com/cvulimeira`,
     },
     {
-        title: 'Digital Innovation One',
-        image: "https://www.projetodraft.com/wp-content/uploads/2019/12/digital-innovation-one.jpg",
-        describe: "É uma plataforma gratuita de cursos online que disponiliza diversos bootcamps - muitos inclusivos - com possibilidades de contratação por grandes empresas.",
-        site: "https://www.dio.me/"
-    }
+        title: `Liestag`,
+        image: `liestag.jpeg`,
+        describe: `Olá, nós somos a Liestag, a sua liga de estágios da Unicamp. Criada para ajudar você a
+        se tornar o profissional do futuro, buscamos desenvolver os alunos nos aspectos das
+        soft skills. Estamos aqui para transformar a sua experiência na universidade,
+        transformar você, no profissional que o mercado tanto procura. Chegue mais, pegue a
+        sua bebida e escolha o seu lugar, temos uma longa jornada pela frente!`,
+        site: `liestag.com`,
+        facebook: `https://www.facebook.com/liestag`,
+        email: ``,
+        instagram: `https://www.instagram.com/liestag`,
+    },
+    {
+        title: `Liga de Data Science`,
+        image: `ligads.jpeg`,
+        describe: `A Liga de Data Science é um grupo de estudos focado em ciência de dados. Nosso principal objetivo é conhecer e ensinar tópicos importantes relacionados a business intelligence, análise de dados e machine learning, além de desenvolver projetos próprios que envolvem esses temas.<br>
+        Os membros da LDS participam do desenvolvimento de projetos seguindo metodologias ágeis ao longo de todo o ano. Além de participarem de momentos em conjunto para estudo, resolução de problemas e desenvolvimento de projetos.<br>
+        Nossas atividades seguem um modelo híbrido, sendo que as atividades presenciais são realizadas na FCA. No entanto, somos formados por estudantes tanto da FT quanto da FCA.<br>
+        Gostaria de conhecer melhor nosso time e nossos projetos? Acesse o link do nosso instagram ou do nosso link tree abaixo.<br>`,
+        site: ``,
+        facebook: ``,
+        email: `linktr.ee/ligadsunicamp`,
+        instagram: `https://www.instagram.com/ligadsunicamp`,
+    },
+    {
+        title: `Liga de Marketing`,
+        image: `ligamkt.png`,
+        describe: `Nós somos compostos por alunos que estudam as ferramentas, tendências,
+        estratégias, campanhas e os novos mercados e cenários do marketing brasileiro e
+        mundial. E para construir uma comunidade no meio universitário mais fidelizada ao
+        mundo do marketing trazemos capacitações internas, treinamentos, eventos gratuitos
+        com gênios de diversas áreas do marketing, entre outros. Estamos abertos a
+        participação de qualquer estudante da FT ou FCA da Unicamp, se quiser saber mais
+        sobre, procure nosso instagram @ligademkt, qualquer dúvida estamos a disposição
+        pela dm!`,
+        site: ``,
+        facebook: ``,
+        email: ``,
+        instagram: `https://www.instagram.com/ligademkt`,
+    },
+    {
+        title: `Mosaico`,
+        image: `mosaico.png`,
+        describe: `Nós somos compostos por alunos que estudam as ferramentas, tendências,
+        estratégias, campanhas e os novos mercados e cenários do marketing brasileiro e
+        mundial. E para construir uma comunidade no meio universitário mais fidelizada ao
+        mundo do marketing trazemos capacitações internas, treinamentos, eventos gratuitos
+        com gênios de diversas áreas do marketing, entre outros. Estamos abertos a
+        participação de qualquer estudante da FT ou FCA da Unicamp, se quiser saber mais
+        sobre, procure nosso instagram @ligademkt, qualquer dúvida estamos a disposição
+        pela dm!`,
+        site: ``,
+        facebook: `https://www.facebook.com/mosaicounicamp`,
+        email: ``,
+        instagram: `https://www.instagram.com/mosa1co`,
+    },
+    {
+        title: `Torque Baja`,
+        image: `torquebaja.jpeg`,
+        describe: `Atualmente a equipe fez algumas adaptações nos trabalhos, por conta do período não
+        presencial, para que o projeto continue sempre progredindo! Houve uma grande
+        intercambialidade entre as áreas, de modo que as administrativas tiveram um enfoque
+        maior. Apesar da dificuldade da área de projetos por não poder frequentar a oficina,
+        houve uma evolução nos conhecimentos da equipe em questões técnicas como
+        simulações e desenhos em softwares, por exemplo.`,
+        site: ``,
+        facebook: `https://www.facebook.com/people/Torque-Baja/pfbid037jdUzAS34FaT8bfhQpShQy33Ub6xZQxzjEjAsjBywWyAFV5ae4jYejxjYXWbBctRl/`,
+        email: ``,
+        instagram: `https://www.instagram.com/equipetorque`,
+    },
+    {
+        title: `Trote da Cidadania`,
+        image: `trote.jpeg`,
+        describe: `"O Trote da Cidadania Unicamp/Limeira, é uma organização estudantil de cunho
+        social, a qual visa aproximar a comunidade de Limeira e a universidade através de
+        eventos voluntários. Trabalhamos para despertar a conscientização do potencial
+        transformador de cada um na sociedade e buscamos refletir cada detalhe de nossas
+        vidas dentro de um contexto global, para tornar o mundo em que vivemos um lugar
+        mais humano, agradável e sustentável para nós e para as próximas gerações.
+        Acreditamos que parte da nossa função social é agradecer a população de Limeira
+        pela hospitalidade ao receber anualmente cerca de 800 novos alunos. Temos em
+        nosso cronograma a semana de recepção de calouros, campanhas para doações,
+        principalmente a de sangue que ocorre anualmente em parceria com o
+        Hemocentro/Campinas; visitas a asilos, creches, ONG’s e centros de reabilitação, além
+        de um projeto que visa o desenvolvimento do senso crítico de crianças através de
+        oficinas semanais. Porém, mesmo com o novo contexto que estamos vivendo, nossas
+        ações não pararam. Nós adaptamos nossas ações para a forma remota, como a
+        recepção dos bixos, campanhas de doações, etc!."`,
+        site: `linktr.ee/TrotedaCidadaniaLimeira`,
+        facebook: ``,
+        email: ``,
+        instagram: ``,
+    },
 ]
 
 var card = document.getElementById("cardProgramas");
