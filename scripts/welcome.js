b805f2aba19795126a134a9c585851acf276de99
Camila Lordelo <camilalordelo15@gmail.com>
2023-02-08 11:54:35 -0300

    Melhorando CSS do header

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,8 +1,6 @@
 var welcome = document.getElementById("welcome");
 
 welcome.innerHTML = `
-<div class="container text-wrap text-white pt-5 pb-5">
-    <b><h1 class="welcome-title";> CALOURADA FT - 2023 </h1></b>
-    <h6 class="welcome-sub-title font-weight: 400;"> Bem vindo, calouro! Este site unifica todas as organizações estudantis da Faculdade de Tecnologia. </h6>      
-</div>
+    <b><h1> CALOURADA FT - 2023 </h1></b>
+    <h6> Bem vindo, calouro! Este site unifica todas as organizações estudantis da Faculdade de Tecnologia. </h6>
 `
